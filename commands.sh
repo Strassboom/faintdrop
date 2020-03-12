084589050e7b6772727235ac4179bcedc690652a
CatherineStrassburg <catherinestrassburg@catheriburgsmbp.propark.com>
2020-03-12 17:34:57 -0400

    Simplified popper.sh to no longer require commands.sh

--- commands.sh
+++ commands.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 sudo grep -qxF './birds.sh' .bashrc || sudo echo './birds.sh' >> .bashrc;sudo chmod +x birds.sh
--- popper.sh
+++ popper.sh
@@ -3,7 +3,8 @@ then
 	echo "-help	Lists arguments"
 	echo "popper.sh [user] [IP-address or hostname]"
 else
-	echo $#
+	#echo $#
 	scp birds.sh $1@$2:
-	cat commands.sh | ssh $1@$2
+	#cat commands.sh "${@}" | ssh $1@$2
+	ssh $1@$2 "echo ${@:3} > repos.txt; tr '\t' '\n' < repos.txt > repos.txt; sudo grep -qxF './birds.sh' .bashrc || sudo echo './birds.sh' >> .bashrc;sudo chmod +x birds.sh"
 fi
